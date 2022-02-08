48ee67e0432dea6709b1965ae835c474434fb476
Guilherme Campello <guilherme.campello@kascosys.com.br>
2022-02-07 22:19:39 -0300

    add solution

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -5,8 +5,21 @@
 # The function is expected to return a STRING.
 # The function accepts STRING brackets as parameter.
 def isBalanced(brackets):
-    # Write your code here
-
+    stack = []
+    
+    for char in brackets:
+        if char in '({[':
+            stack.append(char)
+  
+        if char in '})]':
+            top = stack.pop()
+            if (char == ')' and top != '(') or (char == ']' and top != '[') or (char == '}' and top != '{'):
+                return 'NO'
+    
+    if len(brackets) == 0:
+        return 'NO'
+    else:
+        return 'YES'
 
 if __name__ == '__main__':
 
